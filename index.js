66fadcadb190e3a32db663066a38470919ed0085
Sani Rogers <sanirogers138@gmail.com>
2023-01-10 13:36:51 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(drivers = [], string = ""){
+    const result = drivers.filter(driver => driver.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(drivers = [], string){
+    const result = drivers.filter(driver => driver.startsWith(string));
+    if (result.length >= 0){
+        return result;
+    }
+}
+
+function matchName(drivers = [{name: "", hometown: ""}], string){
+    const result = drivers.filter(driver => driver.name === string)
+    return result;
+}
